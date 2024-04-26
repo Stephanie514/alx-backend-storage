da2df4700aeed78c01d4559944918cba89cb6989
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-26 16:31:50 +0000

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,7 @@
+---SQL script that creates a table users following these requirements:
+---id, integer, never null, auto increment and primary key
+---email, string (255 characters), never null and unique
+---name, string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
