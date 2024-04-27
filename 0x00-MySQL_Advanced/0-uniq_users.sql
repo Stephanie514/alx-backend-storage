decf05ef6a595bf3a82a926b81e0f5837eb63c71
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-27 17:52:41 +0000

    Task 1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,8 @@
 ---SQL script that creates a table users following these requirements:
 ---id, integer, never null, auto increment and primary key
----email, string (255 characters), never null and unique
----name, string (255 characters)
+
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
+	id int AUTO_INCREMENT PRIMARY KEY,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+	);
