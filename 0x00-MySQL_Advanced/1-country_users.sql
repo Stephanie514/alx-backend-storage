0f41f3b9e1fef75948b17eee845c4213a8367b74
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-28 16:19:04 +0000

    In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- SQL script that creates a table users following these requirements:
---With these attributes:
---id, integer, never null, auto increment and primary key
---email, string (255 characters), never null and unique
+-- With these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
 
 CREATE TABLE IF NOT EXISTS users(
 	id int PRIMARY KEY AUTO_INCREMENT,
