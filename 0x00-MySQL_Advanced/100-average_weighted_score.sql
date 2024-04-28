8b299fd2bb944e8bc2fe925e03b84d480b0ebb8a
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-28 00:12:40 +0000

    advanced tasks

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,31 +1,16 @@
 --SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
 --that computes and store the average weighted score for a student.
-
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score DECIMAL(10, 2);
-    DECLARE total_weight DECIMAL(10, 2);
-    
-    -- Calculating total weighted score and total weight
-    SELECT SUM(c.score * p.weight), SUM(p.weight)
-    INTO total_score, total_weight
-    FROM corrections c
-    JOIN projects p ON c.project_id = p.id
-    WHERE c.user_id = user_id;
-    
-    -- Calculating average weighted score
-    DECLARE average_score DECIMAL(10, 2);
-    IF total_weight > 0 THEN
-        SET average_score = total_score / total_weight;
-    ELSE
-        SET average_score = 0;
-    END IF;
-    
-    -- Updating users table with the average weighted score
     UPDATE users
-    SET average_score = average_score
+    SET average_score = (
+        SELECT SUM(c.score * p.weight) / SUM(p.weight)
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = user_id
+    )
     WHERE id = user_id;
 END //
 
--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,17 +1,17 @@
 --SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers
---that computes and store the average weighted score for all students.
+--that computes and store the average weighted score for all student
+
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     UPDATE users u
-    JOIN (
-        SELECT c.user_id, SUM(c.score * p.weight) / SUM(p.weight) AS avg_weighted_score
+    SET average_score = (
+        SELECT SUM(c.score * p.weight) / SUM(p.weight)
         FROM corrections c
         JOIN projects p ON c.project_id = p.id
-        GROUP BY c.user_id
-    ) AS temp ON u.id = temp.user_id
-    SET u.average_score = temp.avg_weighted_score;
+        WHERE c.user_id = u.id
+    );
 END //
 
 DELIMITER ;
