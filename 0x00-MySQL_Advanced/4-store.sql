8f06cd270f6dd244d5cdaade3dd3831e93ebe0ca
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-04-28 16:41:04 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,5 +1,6 @@
---SQL script that creates a trigger that decreases the
---quantity of an item after adding a new order.
+-- SQL script that creates a trigger that decreases the
+-- quantity of an item after adding a new order.
+
 DELIMITER //
 
 CREATE TRIGGER decrease_quantity
