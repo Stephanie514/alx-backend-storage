5082aa06856765efa1c2b515ae1b61d1774cd79f
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-01 20:51:33 +0000

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,18 +9,19 @@ import uuid
 from typing import Union, Callable
 from functools import wraps
 
-
-def count_calls(method: Callable) -> Callable:
+def call_history(method: Callable) -> Callable:
     """
-    Decorator to count how many times a method is called.
+    Decorator to store the history of inputs and outputs for a function.
     """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        key = f"cache_calls:{method.__qualname__}"
-        self._redis.incr(key)
-        return method(self, *args, **kwargs)
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, output)
+        return output
     return wrapper
-
 
 class Cache:
     """
@@ -35,7 +36,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores the input data in Redis and returns the generated key.
@@ -47,8 +48,6 @@ class Cache:
             str: The generated key used to store the data in Redis.
         """
         key = str(uuid.uuid4())
-        self._redis.rpush("inputs", str(data))
-        self._redis.rpush("outputs", key)
         self._redis.set(key, data)
         return key
 
