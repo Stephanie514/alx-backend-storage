fd48291d67bcb24ea9225ce331b1c98c8a01b20c
Stephanie514 <chepkiruistephanie514@gmail.com>
2024-05-01 21:36:19 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 """
 Module: web.py
-Description: Implements a function to retrieve HTML content from a URL and cache the result.
+Description: Implements a function to retrieve HTML content from a URL
+and cache the result.
 """
 
 import requests
 import redis
 
+
 def get_page(url: str) -> str:
     """
-    Retrieve the HTML content of a URL and cache the result with an expiration time of 10 seconds.
+    Retrieve the HTML content of a URL and cache the result with an
+    expiration time of 10 seconds.
 
     Args:
         url (str): The URL to retrieve the HTML content from.
@@ -32,8 +35,3 @@ def get_page(url: str) -> str:
     redis_client.incr(count_key)
 
     return html_content
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/10000/url/http://www.google.com"
-    html_content = get_page(url)
-    print(html_content)
